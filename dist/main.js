33fe1c14b998080a591d67fc559d380c88dd4ec3
karenfbc <k.fuentesb@uniandes.edu.co>
2025-11-21 03:00:24 -0500

    Refactor: moved project out of inner folder

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("@nestjs/common");
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableVersioning({
+        type: common_1.VersioningType.URI,
+        prefix: 'api/v',
+        defaultVersion: '1',
+    });
+    await app.listen(4000);
+}
+bootstrap();
+kamk;
+//# sourceMappingURL=main.js.map
